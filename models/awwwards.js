cffea60b90f46cd6afd5be172ad3dbd5a45f5d62
ktabasov <ktabasov@roboxchange.com>
2018-11-27 15:12:58 +0300

    awwwards site of the day, without as album sending

--- models/awwwards.js
+++ models/awwwards.js
@@ -39,15 +39,65 @@ class Awwwards {
 
       resp.on('end', () => {
         let newDom = new dom().parseFromString(data);
-        let nodes = xpath.select("//div[contains(@id, 'screenshots')]", newDom);
-        let nodes2 = xpath.select("//div[contains(@class, 'box-page-related')]", newDom);
+        let imagepath = xpath.select("//div[contains(@id, 'screenshots')]//img", newDom);
+        let nodes2 = xpath.select("//div[contains(@class, 'box-page-related')]//video//source", newDom);
 
-        debugger
+        this.parseMedia(imagepath, nodes2);
       }).on("error", (err) => {
 
         console.log("Error: " + err.message);
       });
     })
+  }
+
+  static parseMedia(images, video) {
+    let shuffleArr = [];
+
+    for (var i of images) {
+      shuffleArr.push({
+        type: "photo",
+        media: i.attributes[3].nodeValue,
+        caption: '',
+        parse_mode: 'HTML'
+      });
+    }
+
+    for (var j of video) {
+      shuffleArr.push({
+        type: "video",
+        media: j.attributes[0].value,
+        caption: '',
+        parse_mode: 'HTML'
+      });
+    }
+    // images[0].attributes[3].nodeValue
+    // video[0].attributes[0].nodeValue
+    //return shuffleArr;
+
+    this.printMessages(shuffleArr);
+  }
+
+  static printMessages(mediaObject) {
+    bot.sendMessage(process.env.COMMUNITYID, 'Awwwards Site of the day!')
+
+    bot.sendMediaGroup(process.env.COMMUNITYID, [
+      {
+        type: 'photo',
+        media: 'https://assets.awwwards.com/awards/sites_of_the_day/2018/11/kopke-since-1638-1.jpg'
+      },
+      {
+        type: 'photo',
+        media: 'https://assets.awwwards.com/awards/sites_of_the_day/2018/11/kopke-since-1638-1.jpg'
+      },
+      {
+        type: 'video',
+        media: 'https://assets.awwwards.com/awards/external/2018/11/5bfc29cadffb7.mp4'
+      },
+      {
+        type: 'video',
+        media: 'https://assets.awwwards.com/awards/external/2018/11/5bfc29cadffb7.mp4'
+      }
+    ]);
   }
 }
 
